c1d1251b297f92905353c3aec35fedd6eba596c5
SKYXD07 <164731422+SKYXD07@users.noreply.github.com>
2024-03-29 14:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "22152362"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "2df0bf931cc4a8516b43e61263e701b7"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "7029942301"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "Noob_Coderzzz"
+OWNER_ID = "7043794748"
+SUPPORT_GRP = "naksh_accounts"
+UPDATE_CHNL = "best_bots12"
+OWNER_USERNAME = "naksh_hu_vai"
 
